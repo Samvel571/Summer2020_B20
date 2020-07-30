c2ce6f723e5c2688022e08134b4f18a92b769f57
Samvel Hayrapetyan <samohay777@gmail.com>
2020-07-29 23:17:38 -0400

    modified

--- src/practice/replit.java
+++ src/practice/replit.java
@@ -1,4 +1,31 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class replit {
+
+
+    public static void main(String[] args) {
+        String s1 = "12345";
+        String s2 = "abcde";
+
+//        String[] arr1 = s1.split("");
+//        String[] arr2 = s2.split("");
+//
+//        ArrayList<String> list = new ArrayList<>();
+
+        String results = "";
+
+        for (int i = 0; i < s1.length(); i++) {
+
+
+            System.out.println(results);
+
+        }
+
+
+    }
+
 }
+
